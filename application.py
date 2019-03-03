af35fc140a6be78e55d4d671d7a36f5921374b29
AndrewAlexeev <36235185+AndrewAlexeev@users.noreply.github.com>
2019-03-03 23:55:34 +0300

    added module pandas for creating the table

--- application.py
+++ application.py
@@ -4,6 +4,7 @@
 import sys
 import time
 import threading
+import pandas
 from PyQt5  import QtCore
 from PyQt5.QtCore  import QSize,Qt
 
@@ -26,6 +27,8 @@ def processing(signal):
 		i=i+1
 		res = [i,i+1,i+2]
 		time.sleep(5)
+		#res = ex.chek()
+		print("qq");
 		signal.emit(res)# Посылаем сигнал в котором передаём полученные данные
     #!/usr/bin/python3
 # -*- coding: utf-8 -*-
@@ -36,73 +39,66 @@ def processing(signal):
 class Example(QWidget):
 	my_signal = QtCore.pyqtSignal(list, name='my_signal')
 	grid = QGridLayout()
-	
+	colHelp = pandas.Series(['name!','col1!','col2!','col3!'])#Описание колонок
+
+	def chek(self):
+		column = pandas.Series(['name','col1','col2','col3'])#Колонки
+		lines = pandas.Series(['Andrew','Nik','Alex'])#Индексы
+		path = pandas.Series(['C:\\Users\\Andrew','C:\\Users\\Nik','C:\\Users\\Alex'])#Пути
+		df = pandas.DataFrame()
+		for index_ in lines.values:
+			for col in column.values:
+				df.loc[index_,col] = self.FindActFile(1)
+		df['name'] = lines.values
+		return df
+		#print(df)
 	def __init__(self):
 		super().__init__()
 		self.initUI()
-
+	def FindActFile(self,a):
+		return('path')
+	def tableAssembly(self,df,table):#Сборка таблицы
+		
+		table.setColumnCount(len(df.columns))     # Устанавливаем три колонки
+		table.setRowCount(len(df.index))        # и одну строку в таблице
+        # Устанавливаем заголовки таблицы
+		table.setHorizontalHeaderLabels(df.columns.values)
+        # Устанавливаем всплывающие подсказки на заголовки
+		for index,colHelp in enumerate(self.colHelp.values):
+			table.horizontalHeaderItem(index).setToolTip(colHelp)
+        # Устанавливаем выравнивание на заголовки
+		for index,col in enumerate(df.columns):
+			if index == 0:	
+				table.horizontalHeaderItem(index).setTextAlignment(Qt.AlignLeft)
+			elif index == len(df.columns):	
+				table.horizontalHeaderItem(index).setTextAlignment(Qt.AlignLeft)
+			else:	
+				table.horizontalHeaderItem(index).setTextAlignment(Qt.AlignHCenter)
+        # заполняем  строки
+		for index,lin in enumerate(df.index):
+				for index2,col in enumerate(df.columns):
+					table.setItem(index, index2, QTableWidgetItem(str(df.loc[lin,col])))
+        # делаем ресайз колонок по содержимому
+		table.resizeColumnsToContents()
 
 	def initUI(self):
 		quit = QAction("Quit", self)#При выходе из приложения обрабатываем сигнал, чтобы выйти из потока
 		quit.triggered.connect(self.closeEvent)
-		
 		self.setLayout(self.grid)
 		table = QTableWidget(self)  # Создаём таблицу
-		table.setColumnCount(3)     # Устанавливаем три колонки
-		table.setRowCount(1)        # и одну строку в таблице
-        # Устанавливаем заголовки таблицы
-		table.setHorizontalHeaderLabels(["Колонка 1", "Колонка 2", "Колонка 3"])
-        # Устанавливаем всплывающие подсказки на заголовки
-		table.horizontalHeaderItem(0).setToolTip("Column 1 ")
-		table.horizontalHeaderItem(1).setToolTip("Column 2 ")
-		table.horizontalHeaderItem(2).setToolTip("Column 3 ")
-        # Устанавливаем выравнивание на заголовки
-		table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
-		table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
-		table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
-        # заполняем первую строку
-		table.setItem(0, 0, QTableWidgetItem("Text in column 1"))
-		table.setItem(0, 1, QTableWidgetItem("Text in column 2"))
-		table.setItem(0, 2, QTableWidgetItem("Text in column 3"))
- 
-        # делаем ресайз колонок по содержимому
-		table.resizeColumnsToContents()
- 
+		self.tableAssembly(table = table,df = self.chek())
 		self.grid.addWidget(table, 0, 0)   # Добавляем таблицу в сетку
 		# запускаем обработку данных
 		processing(self.my_signal)
- 
         # Обработчик сигнала
 		self.my_signal.connect(self.mySignalHandler, QtCore.Qt.QueuedConnection)
- 
 		self.move(300, 150)
 		self.setWindowTitle('current version')
 		self.show()
 	def mySignalHandler(self, data):  # Вызывается для обработки сигнала
 		#grid = QGridLayout()
 		table = QTableWidget(self)  # Создаём таблицу
-		table.setColumnCount(3)     # Устанавливаем три колонки
-		table.setRowCount(1)        # и одну строку в таблице
-		table.setHorizontalHeaderLabels(["Header 1", "Header 2", "Header 3"])
- 
-        # Устанавливаем всплывающие подсказки на заголовки
-		table.horizontalHeaderItem(0).setToolTip("Column 2 ")
-		table.horizontalHeaderItem(1).setToolTip("Column 2 ")
-		table.horizontalHeaderItem(2).setToolTip("Column 3 ")
- 
-        # Устанавливаем выравнивание на заголовки
-		table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
-		table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
-		table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
- 
-        # заполняем первую строку
-		table.setItem(0, 0, QTableWidgetItem(str(data[0])))
-		table.setItem(0, 1, QTableWidgetItem(str(data[1])))
-		table.setItem(0, 2, QTableWidgetItem(str(data[2])))
- 
-        # делаем ресайз колонок по содержимому
-		table.resizeColumnsToContents()
- 
+		self.tableAssembly(table = table,df =self.chek())
 		self.grid.addWidget(table, 0, 0)   # Добавляем таблицу в сетку
 
 	def closeEvent(self, event):
